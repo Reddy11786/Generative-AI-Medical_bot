4c227a901c96e81dcdb040f1a298ab1d8e77926f
Reddy11786 <vbreddy@cpp.edu>
2024-12-08 21:11:51 -0800

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='Generative AI Project',
+    version='1.0',
+    description='A project to explore and implement generative AI techniques',
+    author='Veerbasant B Reddy',
+    author_email='veerbasantreddy11@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+    
+)
